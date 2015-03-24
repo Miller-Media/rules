fc9ca927812c6fca3788af5a9865461fd3dd563e
Kevin Carwile <kcarwile@gmail.com>
2015-03-24 00:10:14 -0700

    removed unnecessary function from customData extension

--- extensions/rules/Definitions/CustomData.php
+++ extensions/rules/Definitions/CustomData.php
@@ -289,30 +289,4 @@ class _CustomData
 		return $actions;
 	}
 	
-	/**
-	 * Example Operation Callback
-	 *
-	 * Your operation callback will recieve all of the arguments defined in your
-	 * action/condition definition. If an argument is not required, and not provided 
-	 * by the user, then it will be NULL.
-	 *
-	 * Your operation callback will also recieve three additional arguments at the end of
-	 * your regularly defined arguments.
-	 *
-	 * @extraArg1	array				$values		An array of the existing saved values from the configuration form
-	 * @extraArg2	array				$arg_map	A keyed array of the arguments from the event
-	 * @extraArg3	object	\IPS\rules\Action	$operation	The operation object (Action or Condition) which is invoking the callback
-	 *			\IPS\rules\Condition
-	 *
-	 * @return	mixed			If this is a condition callback, you should return either TRUE or FALSE depending on if the condition has passed.
-	 * 					If it is an action callback, you should return a short message to describe the result of the action for debug purposes.
-	 *
-	 * Note: Any value that you return from an operation callback is logged to the debug console when the rule is in debug mode. This way
-	 * it is possible to see what is happening with each operation as it is being evaluated.
-	 */
-	public function operationCallback( $arg1, $values, $arg_map, $operation )
-	{
-		return 'action taken';
-	}
-	
 }
