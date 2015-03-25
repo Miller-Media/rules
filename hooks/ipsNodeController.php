fa34a3383608ff11996a6c07d0b76530206a3679
Kevin Carwile <kcarwile@gmail.com>
2015-03-25 10:01:07 -0700

    ported code from node model to the node controller

---
+++ hooks/ipsNodeController.php
@@ -0,0 +1,31 @@
+//<?php
+
+class rules_hook_ipsNodeController extends _HOOK_CLASS_
+{
+
+	/**
+	 * Get form
+	 *
+	 * @param	\IPS\Node\Model
+	 * @return	\IPS\Helpers\Form
+	 */
+	protected function _addEditForm( \IPS\Node\Model $node )
+	{
+		$form = parent::_addEditForm( $node );
+		
+		if ( \IPS\Db::i()->select( 'COUNT(*)', 'rules_data', array( 'data_class=? AND data_use_mode IN ( \'public\', \'admin\' )', static::rulesDataClass() ) )->first() )
+		{
+			$form->addTab( 'rules_node_data_tab' );
+			foreach ( \IPS\Db::i()->select( '*', 'rules_data', array( 'data_class=? AND data_use_mode IN ( \'public\', \'admin\' )', static::rulesDataClass() ) ) as $row )
+			{
+				$data_field = \IPS\rules\Data::constructFromData( $row );
+				foreach( $data_field->formElements( $this ) as $name => $element )
+				{
+					$form->add( $element );
+				}
+			}
+		}
+		
+		return $form;
+	}
+}
--- hooks/ipsNodeModel.php
+++ hooks/ipsNodeModel.php
@@ -13,6 +13,15 @@ abstract class rules_hook_ipsNodeModel extends _HOOK_CLASS_
 	{
 		parent::form( $form );
 		
+		/**
+		 * Ideally, we should be able to add our form elements here... but unfortunately, nodes built by IPS never bubble this method back up to the
+		 * node model. @Mark at IPS has been kind enough to add a hook point to the node controller that we can use, so that's where this code has been
+		 * relocated to. This will be a problem if somebody wants to call the form() method on the node to build the form outside of the node controller,
+		 * but this will have to do for now.
+		 *
+		 * @see: http://community.invisionpower.com/topic/409061-extending-node-forms/
+		 *
+		 
 		if ( \IPS\Db::i()->select( 'COUNT(*)', 'rules_data', array( 'data_class=? AND data_use_mode IN ( \'public\', \'admin\' )', static::rulesDataClass() ) )->first() )
 		{
 			$form->addTab( 'rules_node_data_tab' );
@@ -25,6 +34,8 @@ abstract class rules_hook_ipsNodeModel extends _HOOK_CLASS_
 				}
 			}
 		}
+		
+		*/
 	}
 
 	/**
